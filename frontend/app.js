087b469b15a7c09785d5e2338cbcce5d0c8b216d
Bruno Cesar <bruno_cco@hotmail.com>
2025-11-14 20:01:29 -0300

    Fix API URL in frontend

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ const taskList = document.getElementById("task-list");
 const form = document.getElementById("task-form");
 const input = document.getElementById("task-input");
 
-const API_BASE_URL = "http://app-cicd-alb-staging-ACCOUNT_ID.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "http://app-cicd-alb-staging-734874965.us-east-1.elb.amazonaws.com";
 const API_URL = `${API_BASE_URL}/tasks`;
 
 function loadTasks() {
