be691991015e0dd9cd4d057877fa35dc5578dccf
Bruno Cesar <bruno_cco@hotmail.com>
2025-11-14 22:07:06 -0300

    Simplificar testes Cypress para apenas 2 testes

--- cypress/e2e/app-test.cy.js
+++ cypress/e2e/app-test.cy.js
@@ -1,55 +1,22 @@
 describe('App CICD E2E Tests', () => {
   beforeEach(() => {
-    // Ignorar todos os erros de aplicação
     cy.on('uncaught:exception', () => false)
-    
     cy.visit('/', { timeout: 30000 })
     cy.get('body', { timeout: 10000 }).should('be.visible')
-    cy.wait(5000)
+    cy.wait(2000)
   })
 
   it('should load the application', () => {
     cy.contains('Task Manager', { timeout: 10000 })
     cy.get('#task-input', { timeout: 10000 }).should('be.visible')
     cy.get('button[type="submit"]', { timeout: 10000 }).should('be.visible')
-    
-    // Testar se a API está respondendo
-    cy.request({
-      method: 'GET',
-      url: 'https://d128yqhncqex8w.cloudfront.net/api/tasks',
-      failOnStatusCode: false
-    }).then((response) => {
-      cy.log('API Response:', response.status)
-    })
   })
 
   it('should create a new task', () => {
-    // Primeiro testar se a API responde
-    cy.request({
-      method: 'GET',
-      url: 'https://d128yqhncqex8w.cloudfront.net/api/tasks',
-      failOnStatusCode: false
-    }).then((response) => {
-      cy.log('API Status:', response.status)
-      
-      if (response.status === 200) {
-        // Se API funciona, tentar criar tarefa
-        const taskTitle = 'Test Task from Cypress'
-        
-        cy.get('#task-input', { timeout: 15000 }).should('be.visible').clear().type(taskTitle)
-        cy.get('button[type="submit"]', { timeout: 10000 }).click()
-        
-        cy.wait(8000)
-        cy.get('body').then(($body) => {
-          if ($body.text().includes(taskTitle)) {
-            cy.contains(taskTitle).should('be.visible')
-          } else {
-            cy.log('Task not found in DOM, but test will pass')
-          }
-        })
-      } else {
-        cy.log('API not responding, skipping task creation test')
-      }
-    })
+    const taskTitle = 'Test Task from Cypress'
+    cy.get('#task-input', { timeout: 10000 }).clear().type(taskTitle)
+    cy.get('button[type="submit"]').click()
+    cy.wait(3000)
+    cy.get('body').should('contain', taskTitle)
   })
 })
